dab4d0c74635293f9014e056eb15f561a1ee5c8a
Yusuke Tanaka <wing0920@gmail.com>
2019-12-23 14:05:10 +0900

    :green_heart: Add test progress confirmation

--- etc/tests/macos.sh
+++ etc/tests/macos.sh
@@ -12,6 +12,8 @@ do
     fi
 done
 
+echo "CLI apps are successfully installed."
+
 GUI_APPS="firefox chrome visual googlejapaneseinput docker alfred slack sourcetree spectacle karabiner-elements alacritty typora 1password chatwork skitch"
 
 for _APP in $GUI_APPS
@@ -21,3 +23,6 @@ do
         exit 1
     fi
 done
+
+echo "GUI apps are successfully installed."
+echo "💚💚💚💚 macOS initialization test ALL GREEN!! 💚💚💚💚"
